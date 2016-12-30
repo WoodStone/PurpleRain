d6bb0e567afc1857e9dccaa7ef36294d20045358
Vestein Dahl <vesteind@stud.ntnu.no>
2016-12-30 23:50:56 +0100

    Small changes to drop class.

--- core/src/main/scala/no/vestein/purplerain/entities/RainDrop.scala
+++ core/src/main/scala/no/vestein/purplerain/entities/RainDrop.scala
@@ -4,17 +4,21 @@ import com.badlogic.gdx.math.Vector2
 
 case class RainDrop(position: Vector2, force: Vector2, size: Float) extends Entity[RainDrop] {
 
-  def applyForce(): RainDrop = {
-    RainDrop(position.x + force.x, position.y + force.y * size, force.x, force.y, size)
+  def applyFunction(f: RainDrop => RainDrop): RainDrop = {
+    f(this)
   }
 
-  def setForce(fx: Float, fy: Float): RainDrop = setForce(new Vector2(fx, fy))
+  def applyForce(): RainDrop = {
+    RainDrop(position.x + force.x, position.y + force.y * size * 2, force.x, force.y, size)
+  }
 
   def setForce(force: Vector2): RainDrop = RainDrop(position, force, size)
 
-  override def setPosition(x: Float, y: Float): RainDrop = setPosition(new Vector2(x, y))
+  def setForce(fx: Float, fy: Float): RainDrop = setForce(new Vector2(fx, fy))
 
-  override def setPosition(pos: Vector2): RainDrop = RainDrop(pos, force, size)
+  def setPosition(pos: Vector2): RainDrop = RainDrop(pos, force, size)
+
+  def setPosition(x: Float, y: Float): RainDrop = setPosition(new Vector2(x, y))
 
 }
 
